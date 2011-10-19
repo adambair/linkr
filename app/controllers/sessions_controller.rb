52e9fe60b0b8b4c0882f3dbec0b9c02ca504858e
Adam Bair <adambair@gmail.com>
2011-10-19 14:46:10 -0400

    Redirect to bookmarks on login and login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to :bookmarks if current_user
   end
 
   def create  
@@ -7,7 +8,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:password])  
       session[:user_id] = user.id  
-      redirect_to dashboard_path, :notice => "Logged in successfully."  
+      redirect_to :bookmarks, :notice => "Logged in successfully."  
     else  
       flash.now.alert = "Invalid login or password"  
       render "new"  
